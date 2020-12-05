8678c9cf46b318b5298d9406ce23d5bf9b09ddd7
Parker Johnson <mrstormthebeach@gmail.com>
2020-12-05 15:14:51 -0700

    add react-table base functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { hot } from "react-hot-loader";
 import "./App.css";
+import Table from './components/table';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1> Hello, World! </h1>
+        <Table />
       </div>
     );
   }
