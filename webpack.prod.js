d759cef54ba15321c80e5cff70d68feaeecfae6b
Parker Johnson <mrstormthebeach@gmail.com>
2020-12-05 22:51:34 -0700

    fix prod webpack setup

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,13 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
 const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
+module.exports = merge(common, {
     mode: 'production',
+    devtool: 'source-map',
+    /*Recc'd by React docs*/
     optimization: {
         minimizer: [new TerserPlugin({ /* additional options here */ })],
     },
-};
+});
+
