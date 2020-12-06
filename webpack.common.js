3afe646d7b73e3162d81d72f2fe6506d3c01652f
Parker Johnson <mrstormthebeach@gmail.com>
2020-12-05 22:31:49 -0700

    webpack overhaul -- add dev and prod configs

---
+++ webpack.common.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.tsx",
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx|js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: "babel-loader",
+                options: { presets: ["@babel/env"] }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader",
+                    {
+                        loader: "css-loader",
+                        options: { modules: true },
+                    }],
+            },
+        ]
+    },
+    resolve: { extensions: ["*", ".ts", ".tsx", ".js", ".jsx"] },
+    output: {
+        path: path.resolve(__dirname, "dist/"),
+        publicPath: "/dist/",
+        filename: "bundle.js"
+    },
+};
