808819da74139bad5bda09a518f53ba89c49ff65
Parker Johnson <mrstormthebeach@gmail.com>
2021-10-04 20:30:24 -0600

    replace prod settings

--- src/http-clients/ff-sleeper-scripts.js
+++ src/http-clients/ff-sleeper-scripts.js
@@ -1,5 +1,4 @@
-// const url = process.env.NODE_ENV === 'production' ? 'https://10guys1cup.com/api' : 'localhost:3001'
-const url = 'http://localhost:3001'
+const url = process.env.NODE_ENV === 'production' ? 'https://10guys1cup.com/api' : 'localhost:3001'
 export default {
     fetchScoreStats: async (seasonID) => {
         console.log('url', url)
